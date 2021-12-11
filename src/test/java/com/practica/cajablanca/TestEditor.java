3fc4680de492e836e889fe187d223c0838b08595
yiiiiiih <92719785+yiiiiiih@users.noreply.github.com>
2021-12-11 19:22:50 +0100

    Update TestEditor.java

--- src/test/java/com/practica/cajablanca/TestEditor.java
+++ src/test/java/com/practica/cajablanca/TestEditor.java
@@ -28,84 +28,104 @@ class TestEditor {
 	@AfterEach
 	void tearDown() throws Exception {
 	}
-
+	//************************** Camino 1: 106-107-108-126 *****************
 	@Test
 	void testnumPalabras1() {
 		assertThrows(IllegalArgumentException.class, () -> {editor.numPalabras(0, 0, "hola");});
 	}
+	
+	//************************** Camino 2:  106-107-109-110-126 *****************
 	@Test
 	void testnumPalabras2() {
 		assertThrows(IllegalArgumentException.class, () -> {editor.numPalabras(2, 10, "hola");});
 	}
+	
+	//************************** Camino 3:  106-107-109-111-112-126 *****************
 	@Test
 	void testnumPalabras3() {
 		editor.leerFichero("vacio.txt");
 		assertEquals(editor.numPalabras(1, 0, "cajablanca"), 0);
 	}
+
+	//************************** Camino 5: 106-107-109-111-112-113-114-115-116-117-118-119-121-123-113-126 *****************
 	@Test
 	void testnumPalabras4() {
-		editor.leerFichero("1linea.txt");
-		
-		assertEquals(editor.numPalabras(1, 1, "a"), 1);
+		editor.leerFichero("cancion.txt");
+		System.out.print(editor.size());
+		assertEquals(editor.numPalabras(1, 2, "I'm"), 1);
 	}
+	
+	//************************** Camino 6: 106-107-109-111-112-113-114-115-116-117-118-121-123-113-126 *****************
 	@Test
 	void testnumPalabras5() {
 		editor.leerFichero("cancion.txt");
-		System.out.print(editor.size());
-		assertEquals(editor.numPalabras(1, 1, "so"), 1);
+		assertEquals(editor.numPalabras(1, 20, "cajablanca"), 0);
 	}
+	
+	//************************** Camino 7: 106-109-111-112-113-114-115-116-117-118-119-121-116-117-118-119-121-123-113-126 *****************
 	@Test
 	void testnumPalabras6() {
 		editor.leerFichero("cancion.txt");
-		assertEquals(editor.numPalabras(1, 20, "cajablanca"), 0);
-	}
-	@Test
-	void testnumPalabras7() {
-		editor.leerFichero("cancion.txt");
 		assertEquals(editor.numPalabras(1, 20, "I"), 11);
 	}
+	
+	//************************** Camino 1: 152-153-154-167 *****************
 	@Test
 	void testmayorLongitud1() throws EmptyCollectionException {
 		assertEquals(editor.mayorLongitud(),null);
 	}
+	
+	//************************** Camino 4: 152-153-154-155-156-157-158-159-160-163-157-155-167 *****************
 	@Test
 	void testmayorLongitud2() throws EmptyCollectionException {
 		editor.leerFichero("1linea.txt");
 		assertEquals(editor.mayorLongitud(),"a");
 	}
+	
+	//************************** Camino 5: 152-153-154-155-156-157-158-159-161-157-158-159-160-157-155-167 *****************
 	@Test
 	void testmayorLongitud3() throws EmptyCollectionException {
 		editor.leerFichero("cancion.txt");
 		assertEquals(editor.mayorLongitud(),"everybody");
 	}
+	
+	//************************** Camino 1: 175-176-197 *****************
 	@Test
 	void testsustituirPalabras1() {
 		editor.leerFichero("vacio.txt");
 		editor.sustituirPalabra("a", "o");
 		assertEquals(editor.size(), 0);
 	}
+	
+	//************************** Camino 2: 175-176-(177-178)-179-180-(181-184)-(192-193)-194-195-197 *****************
 	@Test
 	void testsustituirPalabras2() {
 		editor.leerFichero("vacio1.txt");
 		editor.sustituirPalabra("a", "o");
 		assertEquals(editor.size(), 1);
 	}
+	
+	//************************** Camino 3: 175-176-(177-178)-179-180-(181-184)-185-186-190-(181-184)-(192-193)-194-195-197 *****************
 	@Test
 	void testsustituirPalabras3() {
 		editor.leerFichero("1linea.txt");
 		editor.sustituirPalabra("a", "o");
 		assertEquals(editor.size(), 1);
 	}
+	
+	//************************** Camino 4: 175-176-(177-178)-179-180-(181-184)-(192-193)-194-(181-184)-(192-193)-194-195-197 *****************
 	@Test
 	void testsustituirPalabras4() {
 		editor.leerFichero("1linea.txt");
 		editor.sustituirPalabra("x", "o");
 		assertEquals(editor.size(), 1);
 	}
+	
+	//************************** Camino 5: 175-176-(177-178)-179-180-(181-184)-185-(187-189)-190-(181-184)-(192-193)-194-195-197 *****************
 	@Test
 	void testsustituirPalabras5() {
 		editor.leerFichero("cancion.txt");
-		editor.sustituirPalabra("x", "o");
+		editor.sustituirPalabra("i", "o");
 		assertEquals(editor.size(), 46);
 	}
 }
